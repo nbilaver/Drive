2e393ec7f74520f4d697845be1951ed4e9fd2e39
nbilaver <48863437+nbilaver@users.noreply.github.com>
2021-08-06 22:30:58 -0500

    WIP Changes on mac
    WIP changes on mac, hopefully this works.

--- MacOS/src/main/java/GUI/MainApp.java
+++ MacOS/src/main/java/GUI/MainApp.java
@@ -1,11 +1,14 @@
 package GUI;
 
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
@@ -18,11 +21,7 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        //FXML Version of root
         Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
-
-        //Non-FXML Version of root
-        //Group root = new Group();
 
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
@@ -31,13 +30,6 @@ public class MainApp extends Application {
         Image icon = new Image("images/icon.png");
         stage.getIcons().add(icon);
         stage.setTitle("Drive Sync");
-
-//        //Text
-//        Text l1 = new Text("Testing");
-//        l1.setX(50);
-//        l1.setY(50);
-//        l1.setFill(Color.WHITE);
-//        root.getChildren().add(l1);
 
         stage.setScene(scene);
         stage.show();
